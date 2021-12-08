9a88dc03ee7c841dd12146142a6843453fc1ae0a
eunmokKim <rladmsahr92@naver.com>
2021-12-08 21:55:02 +0900

    Initial commit

---
+++ src/main/java/inflearn_jpa1/JpaOneApplication.java
@@ -0,0 +1,13 @@
+package inflearn_jpa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaOneApplication.class, args);
+    }
+
+}
